753277dfde3a0910043992b71fac1c932b110002
kyurazaki <ewaldosalvage88@gmail.com>
2023-02-09 13:01:34 +0700

    kyurazaki

--- index.php
+++ index.php
@@ -8,35 +8,34 @@ include './database/koneksi.php';
 
 <head>
 
-    <title>Arsip Online - Kediri</title>
+    <title>Arsip Kita - Kediri</title>
 
 </head>
 
-<body class="bg-color-warning" style="background-image: url('./assets/img/arsip.png'); background-size: cover;">
-
+<body class="bg-color-warning" style="background-image: url('./assets/img/background.png'); background-repeat: no-repeat; background-position: center; background-size: cover;">
     <div class="container">
 
         <!-- Outer Row -->
-        <div class="row justify-content-center">
+        <div class="row justify-content-left" style= "padding-left:10px;">
 
-            <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="col-xl-10 col-lg-12 col-md-6">
 
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
+                <div class="card o-hidden shadow-lg my-5">
+                    <div class="card-body p-4">
 
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                        <div class="col-lg-6 d-lg-block bg-primary shadow-lg p-5 text-center">
-                                <h3 class="text-white"><b>Arsip Online <br> Kediri</b></h3>
-                                <img src="./assets/img/undraw_profile.svg" alt="" width = "80%">
+                        <div class="col-lg-6 d-lg-block bg-white p-6 text-center"><br>
+                                <img src="./assets/img/arsip.png" width = "90%"><br><br><br>
+                                <img src="./assets/img/tulisan.png"  width = "55%">
                             </div>
 
-                            <div class="col-lg-6">
-                                <div class="p-5">
+                            <div class="col-lg-5">
+                                <div class="p-4">
 
                             </div>
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Selamat Datang!</h1>
+                                    <div class="text-center"><br>
+                                        <b><h1 class="h3 text-gray-900 mb-4">~ Selamat Datang  ~</b></h1><br><br>
                                     </div>
                                     <form class="user" action="./admin/login.php" method="POST">
                                     <div class="form-group">
@@ -50,11 +49,11 @@ include './database/koneksi.php';
                                                 <input type="checkbox" class="custom-control-input" id="customCheck">
                                             </div>
                                         </div>
-                                        <button class="btn btn-primary btn-user btn-block">Login</button>
+                                        <button class="btn btn-warning btn-user btn-block">Login</button>
                                         </a>
-                                        <hr>
+                                        <br>
                                     </form>
-                                    <hr>
+                                    </br>
                                 </div>
                             </div>
                         </div>
