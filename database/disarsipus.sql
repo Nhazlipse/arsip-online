8a6e6c9050dc21b6f6289f688f4005f972f3a835
nhazlipse <nhazlipse@gmail.com>
2023-02-09 11:32:49 +0700

    update db

--- database/disarsipus.sql
+++ database/disarsipus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 09 Feb 2023 pada 02.26
+-- Waktu pembuatan: 09 Feb 2023 pada 05.31
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.2.0
 
@@ -41,6 +41,36 @@ CREATE TABLE `tb_admin` (
 INSERT INTO `tb_admin` (`id_admin`, `nama_admin`, `username`, `password`) VALUES
 (1, 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3');
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_dataarsip`
+--
+
+CREATE TABLE `tb_dataarsip` (
+  `id_dataarsip` int(10) NOT NULL,
+  `tanggal` date NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `masalah` varchar(100) NOT NULL,
+  `jalan` varchar(100) NOT NULL,
+  `kelurahan` varchar(100) NOT NULL,
+  `kecamatan` varchar(100) NOT NULL,
+  `no_rak` varchar(10) NOT NULL,
+  `no_box` varchar(10) NOT NULL,
+  `kode_klas` varchar(100) NOT NULL,
+  `no_urut` varchar(100) NOT NULL,
+  `nipa` varchar(10) NOT NULL,
+  `tahun` varchar(100) NOT NULL,
+  `keterangan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tb_dataarsip`
+--
+
+INSERT INTO `tb_dataarsip` (`id_dataarsip`, `tanggal`, `nama`, `masalah`, `jalan`, `kelurahan`, `kecamatan`, `no_rak`, `no_box`, `kode_klas`, `no_urut`, `nipa`, `tahun`, `keterangan`) VALUES
+(1, '2023-02-09', 'Sumanto', 'Bangunan', 'jl.Rambutan', 'Kediri', 'Kota', '7', '8', '503', '1', 'BPM', '2020', '503/2012');
+
 --
 -- Indexes for dumped tables
 --
@@ -52,6 +82,12 @@ ALTER TABLE `tb_admin`
   ADD PRIMARY KEY (`id_admin`);
 
 --
+-- Indeks untuk tabel `tb_dataarsip`
+--
+ALTER TABLE `tb_dataarsip`
+  ADD PRIMARY KEY (`id_dataarsip`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -60,6 +96,12 @@ ALTER TABLE `tb_admin`
 --
 ALTER TABLE `tb_admin`
   MODIFY `id_admin` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_dataarsip`
+--
+ALTER TABLE `tb_dataarsip`
+  MODIFY `id_dataarsip` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
