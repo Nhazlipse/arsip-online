620253679ebcb6fc9a50d6eda6161443f40715e0
kyurazaki <ewaldosalvage88@gmail.com>
2023-02-13 10:40:59 +0700

    kyurazaki

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -178,16 +178,16 @@ include './layout/header.php';
 
                         <!-- Earnings (Monthly) Card Example -->
                         <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-primary shadow h-100 py-2">
+                            <div class="card border-left-primary shadow h-100 py-2"style="width:100%; position:relative;top:0%;left: 97%;">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1 text-center"style="font-size:16px;">
                                                 Total Data Arsip</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">10000231</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-900 text-center">10000231</div>
                                         </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                            <i class="fas fa-calendar fa-2x text-gray-500"></i>
                                         </div>
                                     </div>
                                 </div>
@@ -196,16 +196,16 @@ include './layout/header.php';
 
                         <!-- Earnings (Monthly) Card Example -->
                         <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-success shadow h-100 py-2">
+                            <div class="card border-left-success shadow h-100 py-2"style="width:100%; position:relative;top:0%;left: 100%;">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1 text-center"style="font-size:16px";>
                                                 Arsip Dipinjam </div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">3</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-900 text-center";>3</div>
                                         </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                            <i class="fas fa-stopwatch fa-2x text-gray-500"></i>
                                         </div>
                                     </div>
                                 </div>
@@ -227,7 +227,7 @@ include './layout/header.php';
                         </footer>
                         </div> -->
                         <div class="fixed-footer">
-                        <div class="container">Copyright &copy; 2022 Teknotes.id & EtgarKurniawan.com</div>
+                        <div class="container my-auto">Copyright &copy; UNISKA - KEDIRI 2023</div>
                         </div>
                         <!-- End of Footer -->
             
