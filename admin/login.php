89cd79b9de461c77830b2f3d8ae3d0248c32bb3c
rikurunico <rikurunico@gmail.com>
2023-02-25 20:36:25 +0700

    Membenarkan Validasi Ewaldo

--- admin/login.php
+++ admin/login.php
@@ -1,29 +1,57 @@
 <?php
-//panggil koneksi
+// Panggil koneksi
 require_once '../database/koneksi.php';
 
 use database\koneksi;
 
 $koneksi = new koneksi();
 
-
-//deklarasikan user dan pass
+// Deklarasikan user dan pass
 $password = md5($_POST['password']);
 $username = $_POST['username'];
 
-
-$query = "SELECT * FROM tb_admin WHERE username='$username' and  password = '$password'";
+$query = "SELECT * FROM tb_admin WHERE username='$username' AND password='$password'";
 $login = $koneksi->query($query);
-$data = mysqli_fetch_array($login);
+$row = mysqli_fetch_assoc($login);
 
+// Jika tidak ditemukan user/pass
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])) {
+        // Jika captcha response tidak kosong
+        $captcha = $_POST['g-recaptcha-response'];
+        $url = 'https://www.google.com/recaptcha/api/siteverify';
+        $data = [
+            'secret' => '6LfHIrIkAAAAACdbYQDpJocuyEAIgSBPSC1lrWaI',
+            'response' => $captcha
+        ];
+        $options = [
+            'http' => [
+                'header' => 'Content-type: application/x-www-form-urlencoded',
+                'method' => 'POST',
+                'content' => http_build_query($data)
+            ]
+        ];
+        $context = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        $response = json_decode($result);
 
-// jika tidak di temukan user/pass tuser
-if ($data) {
-    session_start();
-    $_SESSION['id_admin'] = $data['id_admin'];
-    $_SESSION['username'] = $data['username'];
-    $_SESSION['password'] = $data['password'];
-    header("location: dashboard.php");
-} else {
-    echo "<script>alert('Login Gagal, Akun tidak ditemukan..!');document.location='../index.php';</script>";
+        if ($response->success && $row) {
+            // Jika captcha dan login berhasil
+            session_start();
+            $_SESSION['id_admin'] = $row['id_admin'];
+            $_SESSION['username'] = $row['username'];
+            $_SESSION['password'] = $row['password'];
+            header("location: dashboard.php");
+            exit;
+        } else {
+            // Jika captcha atau login gagal
+            echo "<script>alert('Login Gagal, Akun tidak ditemukan..!');document.location='../index.php';</script>";
+            exit;
+        }
+    } else {
+        // Jika captcha response kosong
+        $error = 'Please complete the captcha';
+        echo "<script>alert('Silahkan isi captcha');document.location='../index.php';</script>";
+        exit;
+    }
 }
