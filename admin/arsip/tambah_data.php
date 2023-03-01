238f90338e144b82fed5b02296736bd8776577e8
kyurazaki <ewaldosalvage88@gmail.com>
2023-03-01 22:34:26 +0700

    kyurazaki

--- admin/arsip/tambah_data.php
+++ admin/arsip/tambah_data.php
@@ -1,3 +1,5 @@
+<?php include './layout/header.php'; ?>
+
 <?php
 // Validasi Login agar file tidak dapat diakses sebelum melakukan Login Terlebih dahulu
 session_start();
@@ -17,7 +19,6 @@ include './layout/header.php';
 <head>
 
     <title>Arsip Online - Kediri</title>
-</head>
 
 <style>
 label{
@@ -28,6 +29,9 @@ label{
     border-color: black;
 }
 </style>
+
+
+</head>
 
 <body id="page-top">
 
@@ -69,7 +73,7 @@ label{
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Pilih Menu:</h6>
-                        <a class="collapse-item" href="arsip.php">Arsip<b></a>
+                        <a class="collapse-item" href="arsip.php">Arsip</a>
                         <a class="collapse-item" href="../pinjaman/pinjaman.php">Pinjaman</a>
                     </div>
                 </div>
@@ -148,25 +152,22 @@ label{
 
             <!-- card body -->
             <div class="card-body">
-
-            <!-- Tambah Data Arsip Form -->
-            <div class="container">
-            <form action="proses_tambah_data.php" method="post">
-                
+                <div class="container">
+                <form action="proses_tambah_data.php" method="post">
                 <div class="form-group">
                 <label for="judul">Nama Pemilik:</Nama></label>
-                <input type="text" class="form-control" id="nama" name="nama" >
+                <input type="text" class="form-control" id="nama" name="nama" required>
                 </div>
 
                 <div class="form-group">
                 <label for="judul">Masalah:</label>
                 <input type="text" class="form-control" id="masalah" name="masalah"
-                >
+                required>
                 </div>
 
                 <div class="form-group">
                  <label for="judul">Jalan:</label>
-                    <input type="text" class="form-control" id="jalan" name="jalan" >
+                    <input type="text" class="form-control" id="jalan" name="jalan" required>
                  </div>
 
                     <div class="form-group">
@@ -215,34 +216,34 @@ label{
         <div class="form-group">
             <label for="Unit Pengolah">Unit Pengolah:</label>
             <input type="text" class="form-control" id="unit_pengolah" name="unit_pengolah"
-                >
+                required>
         </div>
 
         <div class="form-group">
             <label for="judul">No. Rak:</label>
-            <input type="text" class="form-control" id="no_rak" name="no_rak" >
+            <input type="text" class="form-control" id="no_rak" name="no_rak" required>
         </div>
 
         <div class="form-group">
             <label for="judul">No. Box:</label>
-            <input type="text" class="form-control" id="no_box" name="no_box" >
+            <input type="text" class="form-control" id="no_box" name="no_box" required>
         </div>
 
         <div class="form-group">
             <label for="judul">Kode Klas:</label>
             <input type="text" class="form-control" id="kode_klas" name="kode_klas"
-                >
+                required>
         </div>
 
         <div class="form-group">
             <label for="judul">No. Urut:</label>
             <input type="text" class="form-control" id="no_urut" name="no_urut"
-                >
+                required>
         </div>
 
         <div class="form-group">
             <label for="judul">NIPA:</label>
-            <input type="text" class="form-control" id="nipa" name="nipa" >
+            <input type="text" class="form-control" id="nipa" name="nipa" required>
         </div>
 
         <div class="form-group">
@@ -260,7 +261,7 @@ label{
         <div class="form-group">
             <label for="judul">Keterangan:</label>
             <input type="text" class="form-control" id="keterangan" name="keterangan"
-                >
+                required>
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
